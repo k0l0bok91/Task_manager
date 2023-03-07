a4c096fc73869bd2dd301360598c77ff1f748b7a
Ivan <isaak.njytonovich@gmail.com>
2023-03-07 21:01:57 +0300

    переписал execute_command используя match

---
+++ tests/test_executer.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from executer import *
+
+
+class TetsExecuter(unittest.TestCase):
+    """Тесты для модуля Executer"""
+    def test_execute_command():
+        pass
+    pass
