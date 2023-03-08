448af63e8b830f3c6d587838062fa3a7ba8494eb
Ivan <isaak.njytonovich@gmail.com>
2023-03-09 00:23:19 +0300

    добавил тесты

---
+++ test_executer.py
@@ -0,0 +1,19 @@
+import unittest
+from ..executer import *
+
+
+class TetsExecuter(unittest.TestCase):
+    """Тесты для модуля Executer"""
+
+    def test_generate_first_id(self):
+
+        a = {}
+        self.assertEqual(generate_id(a), '01')
+
+    def test_generate_any_id(self):
+
+        b = {'01':'test'}
+        self.assertEqual(generate_id(b), '02')
+
+        c = {'01':'test', '03':'test', '04':'test', '07':'test'}
+        self.assertEqual(generate_id(c), '08')
