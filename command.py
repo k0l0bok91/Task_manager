dc06520655b9edda48b85c3d8ca67a44f1475713
Ivan <isaak.njytonovich@gmail.com>
2023-03-15 00:27:39 +0300

    перенес _generate_id

--- command.py
+++ command.py
@@ -1,21 +1,14 @@
 import sys
-
+from func_box import FuncBox
 
 class Command():
-    """Абстрактный класс для передаваемых комманд"""
+    """Абстрактный класс для передаваемых аргументов командной строки"""
     def __init__(self, cmd, description=""):
         self.cmd = cmd
         self.description = description
 
     def execute(self, str_args):
         pass
-
-    def print_help(self):
-        print('''
-<show>--Глянуть одним глазком
-<add>--Добавить задачи
-<done>--Пометить выполненой
-        ''')
 
 
 class ParentCommand(Command):
@@ -24,6 +17,11 @@ class ParentCommand(Command):
         """Создается пустой список для передачи в него агрументов командной строки."""
         super().__init__(cmd, value)
         self.children = []
+
+    def pars_args(self):
+        """Считывает аргументы из командной строки возвращает tuple"""
+        str_args = sys.argv
+        return str_args
 
     def add_child(self, str_args):
         """Добавляем в список команду и value"""
@@ -64,11 +62,9 @@ class ArgCommand(Command):
         self.method = method
         self.str_args = sys.argv
 
-    def get_parser(self):
-        str_args = sys.argv
-        return str_args
 
     def execute(command, value):
+        """ """
         match command:
             case 'show':
                 show_tasks()
@@ -80,6 +76,3 @@ class ArgCommand(Command):
                 print_help()
             case _:
                 print("Для справки воспользуйтесь командой help")
-
-    def print_help(self):
-        self.parser.print_help()
--- func_box.py
+++ func_box.py
@@ -1,17 +1,51 @@
+from storage import JSONStorage
+from pathlib import Path
+from tabulate import tabulate
+from command import Command
 
-
-
+to_do_list = JSONStorage.load_file(JSONStorage(Path.cwd() / "data_file.json"))
+command = Command(cmd)
 
 class FuncBox():
-    """Функции работы """
-    def __init__(self):
-        ...
+    """Функции работы приложения"""
+    def __init__(self, command, value, to_do_list): # передаем сюда экземпляр класса Storage -> dict и экземпляр класса Command
+        self.to_do_list = to_do_list
+        self.command = command
+        self.value = value
 
-    def show_tasks():
-        ...
-    def add_tasks():
-        ...
+    def _generate_id(self):
+        """Генерирует уникальный ID задачи"""
+        if self.to_do_list == {}:
+            return f'{1:02}'
+        else:
+            int_key = [int(key) for key in self.to_do_list.keys()]
+            new_id = max(int_key) + 1
+            return f'{new_id:02}'
+
+    def show_tasks(self):
+        """выводит на экран текущие задачи в виде таблицы"""
+        if self.to_do_list == {}:
+            print('Невыполненных задач пока нет. Хорошая работа!')
+        else:
+            print(tabulate(self.to_do_list.items(), headers=['ID', 'Невыполненные задачи'], tablefmt="grid"))
+
+    def add_tasks(self, value): # Принимает параметр value и записывает его в конец словаря
+        """ Добавляет новую задачу в конец словаря """
+        i = self._generate_id()
+        new_task = {i: " ".join(self.value)}
+        self.to_do_list = self.to_do_list | new_task
+        return self.to_do_list
+
     def delete_tasks():
         ...
-    def print_help():
-        ...
+    def print_help(self):
+        print('''
+<show>--Глянуть одним глазком
+<add>--Добавить задачи
+<done>--Пометить выполненой
+        ''')
+
+
+test_case = FuncBox(to_do_list)
+
+test_case.show_tasks()
